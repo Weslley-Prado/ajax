f1fcf310a1b653091a88d62d3dc8deda3215d064
Weslley Prado <weslley.prado73@gmail.com>
2022-02-17 13:06:43 -0300

    Update Ajax

---
+++ server.js
@@ -0,0 +1,44 @@
+const bodyParser = require('body-parser')
+const express  = require('express')
+const app = express()
+
+app.use(express.static('.'))
+//inside folder will go to provide the static files
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json()) //transform the files received in object
+
+
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './upload')
+    },
+    filename: function (req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({ storage }).single('arquivo')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if (err) {
+            return res.end('Ocorreu um erro.')
+        }
+
+        res.end('Concluído com sucesso.')
+    })
+})
+
+app.get('/parOuImpar', (req, res) => {
+    // req.body
+    // req.query
+    // req.params
+    const par = parseInt(req.query.numero) % 2 === 0
+    res.send({
+        resultado: par ? 'par' : 'impar'
+    })
+})
+
+app.listen(8081, () =>console.log('Loading...'))
